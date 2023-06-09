2ae2be145d03f650e066eb22f5ece402fb8ccec0
zengyinning <yinningzeng@163.com>
2023-06-09 15:55:18 +0800

    add hello_window demo

---
+++ hello_window/hello_window/src/main.cpp
@@ -0,0 +1,85 @@
+/*****************************************************************//**
+ * \file   Application.cpp
+ * \brief  generate a window
+ *
+ * \author Flork
+ * \date   March 2023
+ *********************************************************************/
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);  // 版本号
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);  // 版本号
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 核心模式
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // 新建一个GLFW窗口
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    // 注册回调函数
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // 初始化glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD!!" << std::endl;
+        return -1;
+    }
+
+
+    // 渲染循环
+    while (!glfwWindowShouldClose(window))
+    {
+        // 处理输入
+        processInput(window);
+
+        // render
+        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // 交换缓冲区
+        glfwSwapBuffers(window);
+        // 检查有无触发事件
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
